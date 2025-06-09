6b50c31251fca5831c4d5de7bd8e9b1726ac2a84
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2025-06-09 13:50:28 +0200

    Update firebase.client.js

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHwvn6z1b0Hl7lKTCDIa9bRzFNkMKhN0U",
-    authDomain: "inspired-berm-460713-t6.firebaseapp.com",
-    projectId: "inspired-berm-460713-t6",
-    storageBucket: "inspired-berm-460713-t6.firebasestorage.app",
-    messagingSenderId: "67985739597",
-    appId: "1:67985739597:web:1ffac27c3e99a4de783104",
-    measurementId: "G-4LQFHVHMYN"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 const app = initializeApp(firebaseConfig);

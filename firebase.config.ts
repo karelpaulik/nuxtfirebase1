f5c2d9142f806c3fac8edc08ff600bfdb930a635
karel.paulik <karelpaulik@email.cz>
2025-07-04 11:07:40 +0200

    firebase config - into separate file.

---
+++ firebase.config.ts
@@ -0,0 +1,15 @@
+// TENTO SOUBOR MUSÍ BÝT PŘIDÁN DO .gitignore!
+
+interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+
+export const firebaseConfig: FirebaseConfig = {
+
+};
--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -2,15 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: ""
+// };
+
+import { firebaseConfig } from '~/firebase.config'; // Příklad, pokud je v kořeni projektu
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app, "firestore-in-fb-pa1");

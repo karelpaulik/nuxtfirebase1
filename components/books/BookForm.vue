2ac0e25a602812b03ed1b47677074a7fc0197d73
karel.paulik <karelpaulik@email.cz>
2025-10-15 15:34:58 +0200

    FormLayou - nová samostatná komponenta.

--- components/books/BookForm.vue
+++ components/books/BookForm.vue
@@ -1,61 +1,37 @@
 <template>
-  <section class="column border1 relative-position">
-    <div v-if="loading" class="loading-overlay"></div>
+  <FormLayout
+    :loading="loading"
+    :form-id="formId"
+    :has-changes="hasChanges"
+    :error="error"
+    @add-doc="handleAddDoc"
+    @update-doc="handleUpdateDoc"
+    @revert-changes="handleRevertChanges"
+    @del-doc="handleDelDoc"
+    @clear-error="error = null"
+  >
+    <h4 class="q-pa-xs q-ma-xs">{{ formId === 'new' ? 'Nový záznam' : 'Detail záznamu' }}</h4>
 
-    <div v-if="loading" class="absolute-center q-pa-md text-center text-primary z-top">
-      <q-spinner size="3em" color="primary" />
-      <p class="q-mt-sm">Zpracovávám data, prosím čekejte...</p>
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input v-model="formData.title" label="title" />
+      <q-input v-model="formData.author" label="Author" />
+      <DateInput v-model="formData.createdDate" label="createdDate" />
+
+      <InputSelect
+        v-model="formData.currUserRefUsers"
+        label="Vyberte uživatele"
+        collection-name="users"
+        :attributes="userSelectAttributes"
+        :option-value-fn="opt => opt.id"
+        :option-label-fn="opt => `${opt.fName} ${opt.lName} ${opt.born}`"
+      />
+      <!-- 
+      :attributes  Které atributy se uloží do db v rererenci.
+      :option-value-fn  Ponechat téměř vždy "opt.id". I bez "emit-value" a "map-option" je vhodné ponechat. V "multiple" téměř nutné, v "single" není nezbytné - ale stále vhodné (porovnání záznamů).
+      :option-label-fn  Které atributy se zobrazí po vybrání v q-select (podmnožina :attribute)
+      -->
     </div>
-
-    <FormToolbar
-      :form-id="formId"
-      :has-changes="hasChanges"
-      @add-doc="handleAddDoc"
-      @update-doc="handleUpdateDoc"
-      @revert-changes="handleRevertChanges"
-      @del-doc="handleDelDoc"
-    />
-
-    <div class="column">
-      <div class="form-content-scroll q-pa-md">
-
-        <q-banner v-if="error" inline-actions class="text-white bg-negative q-mb-md">
-          <template v-slot:avatar>
-            <q-icon name="warning" color="white" />
-          </template>
-          <p class="q-ma-none">Chyba při zpracování dat: <strong>{{ error.message }}</strong></p>
-          <template v-slot:action>
-            <q-btn flat color="white" label="Zavřít" @click="error = null" />
-          </template>
-        </q-banner>
-
-        <h4 class="q-pa-xs q-ma-xs">{{ formId === 'new' ? 'Nový záznam' : 'Detail záznamu' }}</h4>
-
-        <div class="q-pa-md" style="max-width: 300px">
-          <q-input v-model="formData.title" label="title" />
-          <q-input v-model="formData.author" label="Author" />
-          <DateInput v-model="formData.createdDate" label="createdDate" />
-
-          <InputSelect
-            v-model="formData.currUserRefUsers"
-            label="Vyberte uživatele"
-            collection-name="users"
-            :attributes="userSelectAttributes"
-            :option-value-fn="opt => opt.id"
-            :option-label-fn="opt => `${opt.fName} ${opt.lName} ${opt.born}`"
-          />
-          <!-- 
-          :attributes  Které atributy se uloží do db v rererenci.
-          :option-value-fn  Ponechat téměř vždy "opt.id". I bez "emit-value" a "map-option" je vhodné ponechat. V "multiple" téměř nutné, v "single" není nezbytné - ale stále vhodné (porovnání záznamů).
-          :option-label-fn  Které atributy se zobrazí po vybrání v q-select (podmnožina :attribute)
-          -->
-        </div>
-
-      </div>
-
-    </div>
-
-  </section>
+  </FormLayout>
 </template>
 
 <script setup lang="ts">
@@ -74,7 +50,7 @@ import type { BookFormType } from '@/schemas/bookSchema';
 import { userSelectAttributes } from '@/schemas/userSchema'; // Atributy pro výběr uživatele
 
 // Komponenty
-import FormToolbar from '~/components/_shared/nav/FormToolbar.vue';
+import FormLayout from '~/components/_shared/layout/FormLayout.vue';
 import DateInput from '~/components/_shared/form/DateInput.vue';
 import InputSelect from '~/components/_shared/form/InputSelect.vue'; // Generická komponenta
 
@@ -103,25 +79,4 @@ const {
     confirmLeave: true,
   }
 );
-
-
 </script>
-
-<style scoped>
-.form-content-scroll {
-  overflow-y: auto;
-  max-height: calc(100vh - 400px);
-
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
-
-.form-content-scroll::-webkit-scrollbar {
-  display: none;
-}
-
-.border1 {
-  border: 5px solid gray;
-  border-radius: 5px;
-}
-</style>
--- components/users/UserForm.vue
+++ components/users/UserForm.vue
@@ -1,123 +1,102 @@
 <template>
-  <section class="column border1 relative-position">
-    <div v-if="loading" class="loading-overlay"></div>
-    
-    <div v-if="loading" class="absolute-center q-pa-md text-center text-primary z-top">
-      <q-spinner size="3em" color="primary" />
-      <p class="q-mt-sm">Zpracovávám data, prosím čekejte...</p>
+  <FormLayout
+    :loading="loading"
+    :form-id="formId"
+    :has-changes="hasChanges"
+    :error="error"
+    @add-doc="handleAddDoc"
+    @update-doc="handleUpdateDoc"
+    @revert-changes="handleRevertChanges"
+    @del-doc="handleDelDoc"
+    @clear-error="error = null"
+  >
+    <div class="text-h4 q-pa-xs q-ma-xs">{{ formId === 'new' ? 'Nový záznam' : 'Detail záznamu' }}</div>
+  
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input v-model="formData.fName" label="fName" />
+      <q-input v-model="formData.lName" label="lName" />
+      <q-input v-model="formData.born" label="Born" />
+      <DateInput v-model="formData.createdDate" label="createdDate" />
+      <q-input v-model.number="formData.childrenCount" label="childrenCount" />
+      <q-input v-model.number="formData.userHeight" label="userHeight" @keydown="(event) => usePreventKeys([','], false)(event)" />
     </div>
-    
-    <FormToolbar
+
+    <q-checkbox v-model="formData.hasDrivingLic" label="Řidičský průkaz" />
+
+    <div>
+      <q-checkbox
+        v-for="option in hobbiesOptions"
+        v-model="formData.hobbies"
+        :key="option.value"
+        :val="option.value"
+        :label="option.label"
+      />
+    </div>
+
+    <div>
+      <q-radio
+        v-for="option in pickedOptions"
+        v-model="formData.picked"
+        :key="option.value"
+        :val="option.value"
+        :label="option.label"
+      />
+    </div>
+
+    <div class="q-pa-md text-h4">Hlavní adresa:</div>
+    <AddressForm v-model="formData.mainAddress" />
+
+    <AddressesList v-model="formData.addresses" />
+    <q-separator spaced />
+
+    <div class="q-pa-md text-h4">Nahrát soubory</div>
+    <FileUpload
       :form-id="formId"
-      :has-changes="hasChanges"
-      @add-doc="handleAddDoc"
-      @update-doc="handleUpdateDoc"
-      @revert-changes="handleRevertChanges"
-      @del-doc="handleDelDoc"
+      :collection-name="COLLECTION_NAME"
+      v-model="formData.files"
+      @save-request="handleUpdateDoc(false)"
     />
+    <q-separator spaced class="q-mt-lg" />
 
-    <div class="column">
-      <div class="form-content-scroll q-pa-md">
+    <div class="q-pa-md text-h4">Seznam nahraných souborů</div>
+    <FileList
+      :form-id="formId"
+      :collection-name="COLLECTION_NAME"
+      v-model="formData.files"
+      @save-request="handleUpdateDoc(false)"
+    />
+    <q-separator spaced />
 
-        <q-banner v-if="error" inline-actions class="text-white bg-negative q-mb-md">
-          <template v-slot:avatar>
-            <q-icon name="warning" color="white" />
-          </template>          
-          <p class="q-ma-none">Chyba při zpracování dat: <strong>{{ error.message }}</strong></p>          
-          <template v-slot:action>
-            <q-btn flat color="white" label="Zavřít" @click="error = null" /> 
-          </template>
-        </q-banner>
-        
-        <div class="text-h4 q-pa-xs q-ma-xs">{{ formId === 'new' ? 'Nový záznam' : 'Detail záznamu' }}</div>
-
-        <div class="q-pa-md" style="max-width: 300px">
-          <q-input v-model="formData.fName" label="fName" />
-          <q-input v-model="formData.lName" label="lName" />
-          <q-input v-model="formData.born" label="Born" />
-          <DateInput v-model="formData.createdDate" label="createdDate" />
-          <q-input v-model.number="formData.childrenCount" label="childrenCount" />
-          <q-input v-model.number="formData.userHeight" label="userHeight" @keydown="(event) => usePreventKeys([','], false)(event)" />
-        </div>
-
-        <q-checkbox v-model="formData.hasDrivingLic" label="Řidičský průkaz" />
-
-        <div>
-          <q-checkbox
-            v-for="option in hobbiesOptions"
-            v-model="formData.hobbies"
-            :key="option.value"
-            :val="option.value"
-            :label="option.label"
-          />
-        </div>
-
-        <div>
-          <q-radio
-            v-for="option in pickedOptions"
-            v-model="formData.picked"
-            :key="option.value"
-            :val="option.value"
-            :label="option.label"
-          />
-        </div>
-
-        <div class="q-pa-md text-h4">Hlavní adresa:</div>
-        <AddressForm v-model="formData.mainAddress" />
-
-        <AddressesList v-model="formData.addresses" />
-        <q-separator spaced />
-
-        <div class="q-pa-md text-h4">Nahrát soubory</div>
-        <FileUpload
-          :form-id="formId"
-          :collection-name="COLLECTION_NAME"
-          v-model="formData.files"
-          @save-request="handleUpdateDoc(false)"
-        />        
-        <q-separator spaced class="q-mt-lg" />
-
-        <div class="q-pa-md text-h4">Seznam nahraných souborů</div>
-        <FileList
-          :form-id="formId"
-          :collection-name="COLLECTION_NAME"
-          v-model="formData.files"
-          @save-request="handleUpdateDoc(false)"
-        />
-        <q-separator spaced />
-
-        <div class="q-pa-md text-h4">Debug Data:</div>
-        <p>
-            POZN: formData je reaktivní objekt se Ref pro v-model.
-            formVee.values je interní readonly stav VeeValidate.
-            Oba by měly být synchronní.
-        </p>
-        <div class="bg-grey-2 q-pa-md q-gutter-md">
-          <div>
-            formData (pro v-model):<pre>{{ formData }}</pre>
-          </div>
-          <div>
-            formVee.values (VeeValidate interní):<pre>{{ formVee.values }}</pre>
-          </div>
-          <div>
-            formVee.errors:<pre>{{ formVee.errors }}</pre>
-          </div>
-          <div>
-            formVee.meta:<pre>{{ formVee.meta }}</pre>
-          </div>
-          <div>
-            hasChanges (Vlastní stav):<pre>{{ hasChanges }}</pre>
-          </div>
-        </div>
+    <div class="q-pa-md text-h4">Debug Data:</div>
+    <p>
+        POZN: formData je reaktivní objekt se Ref pro v-model.
+        formVee.values je interní readonly stav VeeValidate.
+        Oba by měly být synchronní.
+    </p>
+    <div class="bg-grey-2 q-pa-md q-gutter-md">
+      <div>
+        formData (pro v-model):<pre>{{ formData }}</pre>
+      </div>
+      <div>
+        formVee.values (VeeValidate interní):<pre>{{ formVee.values }}</pre>
+      </div>
+      <div>
+        formVee.errors:<pre>{{ formVee.errors }}</pre>
+      </div>
+      <div>
+        formVee.meta:<pre>{{ formVee.meta }}</pre>
+      </div>
+      <div>
+        hasChanges (Vlastní stav):<pre>{{ hasChanges }}</pre>
       </div>
     </div>
-  </section>
+  </FormLayout>
 </template>
 
 <script setup lang="ts">
 // Pozn. Ve vue/nuxt nebusí být všechny importy na začátku souboru.
 // Tato podmínka platí pouze pro čisté javascript/typescript soubory.
-import { ref, toRef } from 'vue';
+import { toRef } from 'vue';
 import { usePreventKeys } from '~/composables/usePreventKeys';
 
 import { useDocHandlers } from '~/composables/useDocHandlers';
@@ -126,10 +105,9 @@ import type { UserFormType } from '@/schemas/userSchema';
 import FileUpload from '~/components/_shared/file/FileUpload.vue';
 import FileList from '~/components/_shared/file/FileList.vue';
 
-import type { FileSchemaType } from '@/schemas/fileSchema'; // Import nového typu souboru
 import AddressesList from '~/components/_shared/form/AddressesList.vue'; // Import nové komponenty pro seznam adres
 import AddressForm from '~/components/_shared/form/AddressForm.vue'; // Importujeme novou komponentu pro jednu adresu
-import FormToolbar from '~/components/_shared/nav/FormToolbar.vue';
+import FormLayout from '~/components/_shared/layout/FormLayout.vue';
 import DateInput from '~/components/_shared/form/DateInput.vue';
 
 const props = defineProps<{
@@ -163,24 +141,3 @@ const {
   }
 );
 </script>
-
-<style scoped>
-/* Skrolovatelná část formuláře */
-.form-content-scroll {
-  overflow-y: auto; /* Povolí vertikální skrolování, když obsah přesáhne max-height */
-  max-height: calc(100vh - 400px); /* Přibližný výpočet */
-
-  /* Styly pro skrytí scrollbaru (volitelné, pro čistější vzhled) */
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-}
-
-.form-content-scroll::-webkit-scrollbar {
-  display: none; /* Chrome, Safari, Opera */
-}
-
-.border1 {
-  border: 5px solid gray;
-  border-radius: 5px;
-}
-</style>

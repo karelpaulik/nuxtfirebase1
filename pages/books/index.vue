5718d691f613fc01bac658662cfdd30ca95357a4
karel.paulik <karelpaulik@email.cz>
2025-10-17 14:14:22 +0200

    List - > z pages do samostatné komponenty

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,60 +1,7 @@
 <template>
-  <section>   
-    <div class="fixed z-top">
-      <q-toolbar class="bg-blue-grey text-white shadow-2">
-        <q-btn flat stretch no-caps @click="navigateTo(`/${PAGE_NAME}/new`)" label="Nový záznam" />
-      </q-toolbar>
-    </div>
-
-    <div class="q-pt-xl">
-      <h4 class="q-pa-xs q-mt-none q-mb-none">Všechny dokumenty</h4>
-
-      <p v-if="loading">Načítám data...</p>
-      <p v-else-if="error?.message">Chyba při načítání dat: {{ error.message }}</p>
-      <div v-else-if="documents && documents.length > 0">
-        <ul>
-          <li v-for="doc in documents" :key="doc.id" class="q-pa-xs">
-            <NuxtLink :to="`/${PAGE_NAME}/${doc.id}`">
-              {{ doc.data.title }} {{ doc.data.author }} (Created Date: {{ doc.data.createdDate }}) {{ doc.data.currUserRef?.id }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <p v-else>Žádná data v databázi (nebo žádné výsledky filtru).</p>
-
-
-
-    </div>
-  </section>
+  <BookList />
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
-import { useCollectionHandlers } from '~/composables/useCollectionHandlers';
-
-import { bookApiSchema } from '@/schemas/bookSchema';
-import type { BookApiType } from '@/schemas/bookSchema'; // Typ pro data po validaci
-
-const COLLECTION_NAME = 'books';
-const PAGE_NAME = 'books';
-const API_SCHEMA = bookApiSchema;
-type ApiType = Omit<BookApiType, 'id'>;
-
-const {
-  documents,
-  loading,
-  error,
-  collectionHandlers: { handleReadAllDocs, handleReadFilterDocs },
-} = useCollectionHandlers<ApiType>(COLLECTION_NAME, {
-  validationSchema: API_SCHEMA // <-- Zde se předává schéma. No schema, nebo undefined = no validation.
-});
-
-// Načíst všechny dokumenty při načtení komponenty
-onMounted(() => {
-  handleReadAllDocs();
-  console.log(documents);
-});
-
-
+  import BookList from '~/components/books/BookList.vue';
 </script>

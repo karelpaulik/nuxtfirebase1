b3ecbd66239cea0df34e641c0450895ce9915365
karel.paulik <karelpaulik@email.cz>
2026-01-05 11:43:57 +0100

    Adresa zabalena do: q-expansion-item

--- components/_shared/data/address/AddressForm.vue
+++ components/_shared/data/address/AddressForm.vue
@@ -1,12 +1,22 @@
 //components/AddressForm.vue
 <template>
   <div class="q-gutter-xs">
-    <q-input v-model="item.country" label="Stát" />
-    <q-input v-model="item.city" label="Město" />
-    <q-input v-model="item.street" label="Ulice (nepovinné)" />
-    <q-input v-model="item.houseNr" label="Číslo popisné" />
-    <q-input v-model="item.postalCode" label="PSČ" />
-    <q-input v-model="item.note" label="Poznámka (nepovinné)" />
+    <q-expansion-item
+      switch-toggle-side
+      expand-separator
+      :label="`Stát: ${item.country}`"
+    >
+      <q-card>
+        <q-card-section>
+          <q-input v-model="item.country" label="Stát" />
+          <q-input v-model="item.city" label="Město" />
+          <q-input v-model="item.street" label="Ulice (nepovinné)" />
+          <q-input v-model="item.houseNr" label="Číslo popisné" />
+          <q-input v-model="item.postalCode" label="PSČ" />
+          <q-input v-model="item.note" label="Poznámka (nepovinné)" />
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
   </div>
 </template>
 
